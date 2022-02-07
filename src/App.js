738f7f2a9b4a39ab845c1e0df15106b76986c762
Marnie Kelly <marnie.kelly.123@gmail.com>
2022-02-07 16:20:02 -0500

    fixed css

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>< Title /> </div>
         <div>< Inputs /></div>
       </header>
     </div>
